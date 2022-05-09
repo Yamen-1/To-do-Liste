f2b4b167833dd31976ad0cfad64890daaeef928e
Nathalie Scherf <nascherf@yahoo.se>
2022-05-09 10:53:02 +0200

    setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../css/App.scss';
+import {useState} from "react";
+
+
+function App() {
+  // state mit aufgaben
+  return (
+    <div className="App" id={"App"}>
+      <h1>Meine To dos</h1>
+    </div>
+  );
+}
+
+export default App;
