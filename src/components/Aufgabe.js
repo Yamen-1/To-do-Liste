02a837406153282c68f9d49f146c6c3b450b561b
Nathalie Scherf <nascherf@yahoo.se>
2022-05-10 11:40:30 +0200

    aufgabe erledigt markieren

--- src/components/Aufgabe.js
+++ src/components/Aufgabe.js
@@ -1,7 +1,15 @@
 import './Aufgabe.css'
 const Aufgabe = (props) => {
   return (
-  <li className={`Aufgabe ${props.farbe??  'wennFarbeFehlt' }`} id={props.number} > Deine Aufgabe Nummer {props.number} ist {props.text}</li>
+  <li 
+    className={`Aufgabe ${props.farbe??  'wennFarbeFehlt' }`} 
+    id={props.number} 
+    onClick={() => {
+      // funktion rufen von props, mit id (number) als parameter
+      props.aufgabeAlsErledigtMarkieren(props.number)
+    }}
+  
+  > Deine Aufgabe Nummer {props.number} ist {props.text}</li>
   )
 }
 
--- src/components/ToDoListe.js
+++ src/components/ToDoListe.js
@@ -4,41 +4,60 @@ import Aufgabe from './Aufgabe';
 const ToDoListe = () => {
 
   const [aufgaben, setAufgaben] = useState([
-    { text: "Einkaufen gehen", done: false, id: 1, farbe: 'rot'  },
-    { text: "Eis essen", done: false, id: 2, farbe: 'blau'  },
+    { text: "Einkaufen gehen", done: false, id: 1, farbe: 'rot' },
+    { text: "Eis essen", done: false, id: 2, farbe: 'blau' },
     { text: "Spielen", done: false, id: 3, farbe: 'gelb' },
     { text: "Schlafen", done: false, id: 4, farbe: 'rot' },
     { text: "Programmieren", done: true, id: 5, farbe: 'blau' },
     { text: "Aufstehen", done: false, id: 6 },
   ])
+  // Funktion um done von false auf true zu ändern: 
+  // braucht das Id von Aufgabe
+  const aufgabeAlsErledigtMarkieren = (id) => {
+    // kopie von aufgaben, hier ändern wir
+    let geänderteKopie = aufgaben.map(aufgabe => {
+      // // das element auf dem wir geklickt haben finden!
+      if(aufgabe.id === id){
+         // done von false zu true
+         aufgabe.done = true
+        return aufgabe
+      } else {
+        return aufgabe
+      }
+    })
+    // setAufgaben, um die neue Array in state zu speichern:
+    setAufgaben(geänderteKopie)
+  }
 
   return (
     <div className='ToDoListe'>
       <h2>Das sind meine Aufgaben:</h2>
       <ul>
         {/* Schritt 2: nur nicht erledigte aufgaben anzeigen */}
-        { aufgaben.filter((aufgabe) => !aufgabe.done).map((aufgabe) => {
-          return <Aufgabe 
-          text={aufgabe.text} 
-          key={aufgabe.id} 
-          number={aufgabe.id} 
-          // Schritt 3: onClick um als erledigt zu markieren
-          farbe={aufgabe.farbe} />
+        {aufgaben.filter((aufgabe) => !aufgabe.done).map((aufgabe) => {
+          return <Aufgabe
+            text={aufgabe.text}
+            key={aufgabe.id}
+            number={aufgabe.id}
+            // Schritt 3: onClick um als erledigt zu markieren
+            aufgabeAlsErledigtMarkieren={aufgabeAlsErledigtMarkieren}
+            farbe={aufgabe.farbe} />
         })}
       </ul>
       <h2>Das habe ich schon erledigt:</h2>
       {/* Schritt 1: nur erledigte aufgaben anzeigen; Ausfilter wo done == true */}
       {/*Geht auch: { aufgaben.filter((aufgabe) => aufgabe.done )} */}
       {/* [{ text: "Programmieren", done: true, id: 5, farbe: 'blau' }] */}
-       { aufgaben.filter((aufgabe) => aufgabe.done === true ).map(aufgabe => {
-         return <Aufgabe 
-         text={aufgabe.text} 
-         key={aufgabe.id} 
-         number={aufgabe.id} 
-         // Schritt 3: onClick um als erledigt zu markieren
-         farbe={aufgabe.farbe} />
-       }) }
-   
+      {aufgaben.filter((aufgabe) => aufgabe.done === true).map(aufgabe => {
+        return <Aufgabe
+          text={aufgabe.text}
+          key={aufgabe.id}
+          number={aufgabe.id}
+          // Schritt 3: onClick um als erledigt zu markieren
+          aufgabeAlsErledigtMarkieren={aufgabeAlsErledigtMarkieren}
+          farbe={aufgabe.farbe} />
+      })}
+
 
 
 
