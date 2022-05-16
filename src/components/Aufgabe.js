a11934a0002436cbdd14265dffe6d1ac05a1b747
Nathalie Scherf <nascherf@yahoo.se>
2022-05-16 11:20:46 +0200

    props destrukturieren

--- src/components/Aufgabe.js
+++ src/components/Aufgabe.js
@@ -1,20 +1,20 @@
 import './Aufgabe.css'
 import { useState } from 'react'
 
-
-const Aufgabe = (props) => {
+// schritt 1: destrukturieren von Props: Eigenschaften von Objekt holen
+const Aufgabe = ({text, farbe, done, number, aufgabeAlsErledigtMarkieren, removeAufgabe, editAufgabe }) => {
 
   const [edit, setEdit] = useState(false)
-  const [currentVal, setCurrentVal] = useState(props.text)
+  const [currentVal, setCurrentVal] = useState(text)
 
   return (
   <li 
-    className={`Aufgabe ${props.farbe??  'wennFarbeFehlt' }`} 
-    style={{ backgroundColor:  props.done ? ('gray') : ('red')}}
-    id={props.number}
+    className={`Aufgabe ${farbe??  'wennFarbeFehlt' }`} 
+    style={{ backgroundColor:  done ? ('gray') : ('red')}}
+    id={number}
   >
     {
-      (edit === false)? <span>{props.text}</span>
+      (edit === false)? <span>{text}</span>
         :<input value={currentVal}
                 onChange={
                   (event)=>{
@@ -24,15 +24,15 @@ const Aufgabe = (props) => {
     }
     <div>
       <button onClick={() =>{
-        props.aufgabeAlsErledigtMarkieren(props.number)
+        aufgabeAlsErledigtMarkieren(number)
       }
-      }>{(props.done === false)?"erledigen":"zurücksetzen"}</button>
+      }>{(done === false)?"erledigen":"zurücksetzen"}</button>
       <button onClick={() => {
-        props.removeAufgabe(props.number)
+        removeAufgabe(number)
       }}>Delete</button>
       <button onClick={() => {
         setEdit(!edit)
-        props.editAufgabe(props.number, currentVal)
+        editAufgabe(number, currentVal)
       }
       }>{
         (edit === false)?"Edit":"OK"
