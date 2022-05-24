baa2ad68a26689414f391b3d5c114db090a13081
Yamen Alashkar <yamenys.alashkar@gmail.com>
2022-05-24 13:40:59 +0200

    todo

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,39 +1,39 @@
-// 1: Context erstellen: 
+// 1: Context erstellen:
 
 import React, { createContext, useState } from "react";
 
-export const ThemeContext = createContext()
+export const ThemeContext = createContext();
 
-//Komponente: wie soll der Kontext aussehen: 
+//Komponente: wie soll der Kontext aussehen:
 
 const ThemeContextProvider = (props) => {
-// theme/layout für die App. Definieren und ändern können
-  const [ appLayout, setAppLayout ] = useState({
+  // theme/layout für die App. Definieren und ändern können
+  const [appLayout, setAppLayout] = useState({
     istHell: true,
     hell: {
-      text: '#555', 
-      hintergrund: '#ddd',
+      text: "#555",
+      hintergrund: "#ddd",
     },
     dunkel: {
-      text: '#ddd',
-      hintergrund: '#333'
-    }
-  })
+      text: "#ddd",
+      hintergrund: "#333",
+    },
+  });
 
-  // funktion zum Layout ändern: 
+  // funktion zum Layout ändern:
 
   const toggleLayout = () => {
-    // state ändern: alles aus layout, istHell: wenn istHell true ist, dann wird es zu false (!), 
-    setAppLayout({...appLayout, istHell: !appLayout.istHell })
-  }
-// return: Komponente: Teil von ThemeContext Provider 
-// value: ist ein Objekt mit dem Theme 
-  return(
+    // state ändern: alles aus layout, istHell: wenn istHell true ist, dann wird es zu false (!),
+    setAppLayout({ ...appLayout, istHell: !appLayout.istHell });
+  };
+  // return: Komponente: Teil von ThemeContext Provider
+  // value: ist ein Objekt mit dem Theme
+  return (
     <ThemeContext.Provider value={{ appLayout, toggleLayout }}>
       {/* Kinder von ThemeContext Provider werden hier eingesetzt */}
       {props.children}
     </ThemeContext.Provider>
-  )
-}
+  );
+};
 
-export default ThemeContextProvider
+export default ThemeContextProvider;
